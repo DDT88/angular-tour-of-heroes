be1c013532aa78b10c531172226440df77b250c5
daniele deltoso <daniele.deltoso@gmail.com>
2020-10-29 13:10:50 +0100

    commit Add NAvitagion

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -25,10 +25,11 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void{
+   onSelect(hero: Hero): void{
     this.selectedHero = hero;
     this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
+
 
   getHeroes(): void{
     this.heroService.getHeroes()
